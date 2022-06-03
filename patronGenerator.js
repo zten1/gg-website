ca037ac9e1eb0e9afb3f7465d85bdfd97ca363a2
wictor arvidsson <wictor.arvidsson@hotmail.com>
2022-06-03 22:05:34 +0200

    patreon script completed

--- patronGenerator.js
+++ patronGenerator.js
@@ -1,25 +1,26 @@
-
 fetch("./patrons.json")
 .then(response => {
    return response.json();
 })
 .then(jsondata => {
-  //do stuff here
 
   document.getElementById("patrons").innerHTML = ` 
   ${jsondata.map(function(patron) {
-    
-    //convert name to UUID with mojang API
-
-    //get profile img with UUID with API
+  
+    var imgURL = "https://mc-heads.net/avatar/" + patron.minecraft_user_name;
 
     return `
     <div class="patron-profile"> 
+      <img src="${imgURL}">
       <h1>${patron.minecraft_user_name}</h1>
       <p> Since: ${patron.start_date}</p>
     </div> `
 
   }).join('')}
-
   `
 });
+
+
+
+
+
