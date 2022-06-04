ff29ebd20db758217e985300e8e3fd4f87f585e9
wictor arvidsson <wictor.arvidsson@hotmail.com>
2022-06-04 19:01:12 +0200

    some screen size compatability

---
+++ staffGenerator.js
@@ -0,0 +1,21 @@
+fetch("./staffPeople.json")
+.then(response => {
+   return response.json();
+})
+.then(jsondata => {
+
+  document.getElementById("staff-content-box").innerHTML = ` 
+  ${jsondata.map(function(staff) {
+  
+    var imgURL = "https://mc-heads.net/avatar/" + staff.user_name;
+    console.log(staff.user_name)
+    return `
+    <div class="staff-profile">
+        <img src="${imgURL}">
+        <h1>${staff.user_name}</h1>
+        <p>${staff.description} </p>
+    </div>
+    `
+  }).join('')}
+  `
+});
