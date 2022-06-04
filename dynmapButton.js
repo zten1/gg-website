7ccbe0aacdfa4fda763a5d37af104a168f30a5af
wictor arvidsson <wictor.arvidsson@hotmail.com>
2022-06-05 01:55:08 +0200

    dynmap modal

---
+++ dynmapButton.js
@@ -0,0 +1,21 @@
+var modal = document.getElementsByClassName("dynmap-container")[0];
+
+var span = document.getElementsByClassName("dynmap-close")[0];
+
+var dynmap_background = document.getElementsByClassName("dynmap-background")[0];
+
+var btn = document.getElementById("dynmap-button");
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+window.onclick = function(event) {
+  if (event.target == dynmap_background) {
+    modal.style.display = "none";
+  }
+}
