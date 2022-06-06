09861720c6818d630f010cd3dd2474256dc9c827
wictor arvidsson <wictor.arvidsson@hotmail.com>
2022-06-06 16:03:57 +0200

    autoscroll

---
+++ autoscroll.js
@@ -0,0 +1,10 @@
+if (window.location.pathname != "/index.html"){
+    window.onload = function(){
+        window.scroll({
+            top: 600,
+            left: 0,
+            behavior: 'smooth'
+          });
+      }
+}
+
